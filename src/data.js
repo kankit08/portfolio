fc4fa9afce9e983a9d7abf43d5cd2c433767e24e
kankit08 <kankit08@gmail.com>
2022-04-17 17:12:47 +0530

    data updated

--- src/data.js
+++ src/data.js
@@ -2,31 +2,31 @@ export const products = [
   {
     id: 1,
     img: "https://images.pexels.com/photos/7915280/pexels-photo-7915280.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-    link: "http://lama.dev",
+    link: "https://kumarankit.netlify.app/",
   },
   {
     id: 2,
     img: "https://images.pexels.com/photos/7915228/pexels-photo-7915228.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-    link: "http://lama.dev",
+    link: "https://kumarankit.netlify.app/",
   },
   {
     id: 3,
     img: "https://images.pexels.com/photos/7915432/pexels-photo-7915432.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-    link: "http://kumar.dev",
+    link: "https://kumarankit.netlify.app/",
   },
   {
     id: 4,
     img: "https://images.pexels.com/photos/7915284/pexels-photo-7915284.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-    link: "http://lama.dev",
+    link: "https://kumarankit.netlify.app/",
   },
   {
     id: 5,
     img: "https://images.pexels.com/photos/7915243/pexels-photo-7915243.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-    link: "http://lama.dev",
+    link: "https://kumarankit.netlify.app/",
   },
   {
     id: 6,
     img: "https://images.pexels.com/photos/7915379/pexels-photo-7915379.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-    link: "http://lama.dev",
+    link: "https://kumarankit.netlify.app/",
   },
 ];
